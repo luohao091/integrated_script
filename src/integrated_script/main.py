973c4d241aca40f5d07b22e745d8900fe26395da
luohao091 <luohao.622@gmail.com>
2025-08-19 15:50:05 +0800

    chore: bump version to 2.0.0

--- src/integrated_script/main.py
+++ src/integrated_script/main.py
@@ -47,7 +47,7 @@ def setup_argument_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         '--version', 
         action='version', 
-        version='%(prog)s 1.0.1'
+        version='%(prog)s 2.0.0'
     )
     
     parser.add_argument(
